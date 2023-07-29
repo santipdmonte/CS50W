5e6a335951e57e351c048eb0f017d9228efcf086
santipdmonte <santiagopedemonte02@gmail.com>
2023-07-29 16:12:06 -0300

    Priject 1 - Wiki: Search complete

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -3,13 +3,34 @@ from django.shortcuts import render
 from . import util
 
 def index(request):
+    entries = util.list_entries()
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = util.get_entry(title)
+
+        # If true the title exists
+        if description:
+            desc_html = util.get_html(description)
+            return render(request, "encyclopedia/entry.html",{
+                "title": title,
+                "description": desc_html
+            })
+        
+        # Find similar titles
+        entries = [entry for entry in entries if title.lower() in entry.lower()] 
+        return render(request, "encyclopedia/index.html", {
+            "entries": entries
+        })
+        
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
 def entry(request, title):
     description = util.get_entry(title)
-    desc_html = util.get_html(description)
+    if description:
+        desc_html = util.get_html(description)
 
     return render(request, "encyclopedia/entry.html", {
         "title": title,
