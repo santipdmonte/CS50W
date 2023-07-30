7ddb6fddae1fc6ef17ae685d52229df5e1b733ea
santipdmonte <santiagopedemonte02@gmail.com>
2023-07-30 19:35:58 -0300

    Project 1 - Wiki: Edit Page working, variable 'description' change to 'content'

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.http import JsonResponse
+
 from . import util
 
 def index(request):
@@ -7,14 +9,14 @@ def index(request):
 
     if request.method == 'POST':
         title = request.POST.get('title')
-        description = util.get_entry(title)
+        content = util.get_entry(title)
 
         # If true the title exists
-        if description:
-            desc_html = util.get_html(description)
+        if content:
+            cont_html = util.get_html(content)
             return render(request, "encyclopedia/entry.html",{
                 "title": title,
-                "description": desc_html
+                "cont_html": cont_html
             })
         
         # Find similar titles
@@ -28,18 +30,32 @@ def index(request):
     })
 
 def entry(request, title):
-    description = util.get_entry(title)
-    if description:
-        desc_html = util.get_html(description)
+    editing = False
+    if request.method == 'POST':
+        editing = True
+
+        new_content = request.POST.get('new_content')
+        save_edit = request.POST.get('save_edit')
+
+        if save_edit:
+            util.save_entry(title, new_content)
+            editing = False
+
+    content = util.get_entry(title)
+    if content:
+        cont_html = util.get_html(content)
+        print(f"{cont_html=}")
         return render(request, "encyclopedia/entry.html", {
             "title": title,
-            "description": desc_html
+            "cont_html": cont_html,
+            "content": content,
+            "editing": editing
         })
     
     print("NOT TITLE FOUND")
     entries = util.list_entries()
     return render(request, "encyclopedia/index.html", {
-        "entries": entries
+        "entries": entries,
         })
 
 def newpage(request):
@@ -48,7 +64,11 @@ def newpage(request):
         title = request.POST.get('title')
         content = request.POST.get('content')
 
-        # Validations ################################################
+        # Validation
+        cont_validation = util.get_entry(title)
+        if cont_validation:
+            error_message = "Exists a page with this title"
+            return JsonResponse({'error': error_message})
 
         print(f"Saved. {title=} {content=}")
         util.save_entry(title, content)
