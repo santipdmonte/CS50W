efb4499538ac25b8e5ce13c85002ec6cdcd8153c
santipdmonte <santiagopedemonte02@gmail.com>
2023-08-02 18:28:03 -0300

    Project 2: commerce - Create Modeles

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Category, Auction_listing, Bid, Comment
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Auction_listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
+
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,50 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Auction_listing(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    starting_bid = models.IntegerField()
+    image_url = models.CharField(max_length=256)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category", null = True)
+    active = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="winner", null=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+class Bid(models.Model):
+    id = models.AutoField(primary_key=True)
+    amount = models.IntegerField()
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
+    listing = models.ForeignKey(Auction_listing, on_delete=models.CASCADE, related_name="listing_bid")
+
+    def __str__(self):
+        return f"{self.bidder}: {self.listing} ${self.amount}"
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    comment = models.CharField(max_length=256)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    listing = models.ForeignKey(Auction_listing, on_delete=models.CASCADE, related_name="listing_comment")
+
+    def __str__(self): 
+        return f"{self.commenter}: {self.comment}"
+
+
+
+
+
+
+
+
