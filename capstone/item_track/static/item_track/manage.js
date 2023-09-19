b46dcdc3e8f78e09f326dbf24eb4e5d749a07e40
santipdmonte <santiagopedemonte02@gmail.com>
2023-09-19 09:26:48 -0300

    Final Project - Capstone: Manage items, add current stock

--- capstone/item_track/static/item_track/manage.js
+++ capstone/item_track/static/item_track/manage.js
@@ -6,27 +6,38 @@ document.addEventListener('DOMContentLoaded', function() {
     inputPrice = document.getElementById('addPrice');
     inputBarcode = document.getElementById('addBarcode');
     inputAddAmount = document.getElementById('addAmount');
+    currentStock = document.getElementById('currentStock');
     
     // Add an event listener to the inputItem element
     inputItem.addEventListener('input', function() {
         const selectedItem = inputItem.value;
         const option = document.querySelector(`#datalistOptions option[value="${selectedItem}"]`);
 
-        // Save as value the item_id
-        inputItem_id.value = option.dataset.item_id;
-        inputBarcode.value = option.dataset.barcode;
 
+        
         // When found the item, focus on the amount input
         if (option){
             inputAddAmount.focus();
+            // Save as value the item_id
+            inputItem_id.value = option.dataset.item_id;
+            inputBarcode.value = option.dataset.barcode;
         }
 
+        // Manage Stock input
+        if (option && option.dataset.stock) {
+            currentStock.value = option.dataset.stock;
+        } else {
+            currentStock.value = '';
+        }
+
+        // Manage Price input
         if (option && option.dataset.price) {
             inputPrice.value = option.dataset.price;
         } else {
             inputPrice.value = '';
         }
 
+        // Manage Barcode input
         if (option && option.dataset.barcode) {
             inputBarcode.value = option.dataset.barcode;
         } else {
@@ -36,8 +47,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     inputBarcode.addEventListener('input', function() {
         const selectedBarcode = inputBarcode.value;
-
-        console.log(selectedBarcode);
         
         fetch(`/item/barcode/${selectedBarcode}`)
         .then(response => {
@@ -45,6 +54,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 inputItem.value = '';
                 inputItem_id.value = '';
                 inputPrice.value = '';
+                currentStock.value = '';
                 throw new Error('No se encontró el artículo.');
             }
             return response.json();
@@ -55,6 +65,7 @@ document.addEventListener('DOMContentLoaded', function() {
               inputItem.value = item.name;
               inputItem_id.value = item.id;
               inputPrice.value = item.price;
+              currentStock.value = item.stock;
               inputAddAmount.focus();
               console.log(item);
             } 
@@ -106,6 +117,7 @@ function cancelChanges(){
     inputItem_id.value = '';
     inputPrice.value = '';
     inputBarcode.value = '';
+    currentStock.value = '';
     document.getElementById('addAmount').value = 0;
 
     console.log(inputBarcode);
